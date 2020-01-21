48e30fe0be25bfe0b14c558ed08c50a18de15ce9
Smolevich <smolevich90@gmail.com>
2020-01-22 02:09:17 +0300

    Add new configuration and change workflow file

--- lib/Services/GitHub.php
+++ lib/Services/GitHub.php
@@ -14,15 +14,34 @@ abstract class GitHub {
   static function getConfiguration(array $environment): Configuration {
     $commitSha = $environment['GITHUB_SHA'] ?? '';
     $repository = $environment['GITHUB_REPOSITORY'] ?? '';
-
+    $jobId = $commitSha;
     $gitRef = $environment['GITHUB_REF'] ?? '';
     $gitRegex = '#^refs/\w+/#';
+    $eventName = $environment['GITHUB_EVENT_NAME'];
+
+    if ($eventName === 'pull_request') {
+      $event = static::getEvent($environment['GITHUB_EVENT_PATH']);
+      $prNumber = (string)$event['number'];
+      $jobId = sprintf('%s-PR-%s', $commitSha, $prNumber);
+    }
 
     return new Configuration([
       'commit_sha' => $commitSha ?? null,
       'service_branch' => preg_match($gitRegex, $gitRef) ? preg_replace($gitRegex, '', $gitRef) : null,
       'service_build_url' => $commitSha && $repository ? "https://github.com/$repository/commit/$commitSha/checks" : null,
-      'service_name' => 'github'
+      'service_name' => 'github',
+      'service_job_id' => $jobId,
+      'service_pull_request' => $prNumber ?? null,
     ]);
+  }
+
+  static function getEvent(string $path): array {
+    $data = file_get_contents($path);
+
+    if ($data) {
+      return json_decode($data, true) ?: [];
+    }
+
+    return [];
   }
 }
